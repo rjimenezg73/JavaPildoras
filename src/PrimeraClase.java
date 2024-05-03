95c1e0352e99c75dbff454d598e9945b5be26a2a
rjimenezg73 <106135874+rjimenezg73@users.noreply.github.com>
2024-05-03 02:15:24 -0600

    Primera clase - Conf. de Eclipse

---
+++ src/PrimeraClase.java
@@ -0,0 +1,12 @@
+
+public class PrimeraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hola alumnos");
+		System.out.println("Un curso más de Java");
+
+	}
+
+}
