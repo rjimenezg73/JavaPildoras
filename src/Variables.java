f650d854013b000ded39a35af6670204da07593e
rjimenezg73 <106135874+rjimenezg73@users.noreply.github.com>
2024-05-04 01:21:29 -0600

    Clase Variables

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte edad;
+		edad = 51;
+		
+		int edad2 = 44; //Declarar y asignar en la misma línea
+		
+		System.out.println(edad);
+		System.out.println(edad2);
+	}
+
+}
