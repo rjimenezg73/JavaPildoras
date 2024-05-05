45dbd528d21577973c6377b6aab05387c38bc85c
rjimenezg73 <106135874+rjimenezg73@users.noreply.github.com>
2024-05-04 23:19:57 -0600

    Control Flow Program

---
+++ src/CtrlFlow.java
@@ -0,0 +1,13 @@
+
+public class CtrlFlow {
+	 public static void main(String[] args) {
+	        
+	        for(int x = 0 ;x <= 100 ; x++ ) {
+
+	            if(x % 2 != 0  ) {
+	                System.out.println(/*add odd number here*/x + " I'm a Odd number");
+	            }
+	        }
+
+	    }
+}
